14c1ad68e4e48b177131385d28095aa20119d836
fangwei <fangwei@ucfgroup.com>
2019-01-03 14:30:54 +0800

    update

---
+++ src/views/Qrcode.vue
@@ -0,0 +1,19 @@
+<style>
+
+</style>
+<template>
+  <div>
+    <!-- <img style="width:300px;" src="../assets/merge.jpeg"> -->
+    
+    <img style="width:100px;position:relative;top:150px;" src="../assets/222.png">
+    <img style="width:100px" src="../assets/111.png">
+
+    <!-- <img style="width:300px" src="../assets/merge-2.jpeg"> -->
+
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
